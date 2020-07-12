269a88d40bc3f2255c019613a2544b37a107fd74
graceFor <tbgusrud12@gmail.com>
2020-07-12 17:37:02 +0900

    [2.6] DOM - If else - Function practice

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const title = document.querySelector("#title");
 
+const BASE_COLOR = "red";
+const OTHER_COLOR = "#7f8c8d";
+
 function handleClick() {
-  title.style.color = "blue";
+  const currentColor = title.style.color;
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
 }
-title.addEventListener("click", handleClick);
+
+function init() {
+  title.style.color = BASE_COLOR;
+  title.addEventListener("mouseenter", handleClick);
+}
+init();
